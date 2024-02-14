0ff88bd66ab2ad10195752850b1fdc9323dcb255
Esakkyrajam <esakkyrajam@gmail.com>
2024-02-14 22:13:51 +0530

    projects added

--- script.js
+++ script.js
@@ -13,3 +13,40 @@ function toggleMenu(){
     icon.classList.toggle("open");
 }
   
+
+// let slideIndex = 1;
+//   showSlides(slideIndex);
+
+//   function moveCarousel(n) {
+//     showSlides(slideIndex += n);
+//   }
+
+//   function showSlides(n) {
+//     let i;
+//     const slides = document.getElementsByClassName("details-container");
+//     if (n > slides.length) { slideIndex = 1 }
+//     if (n < 1) { slideIndex = slides.length }
+//     for (i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none";
+//     }
+//     slides[slideIndex - 1].style.display = "block";
+//   }
+
+
+// let slideIndex = 0;
+// showSlides();
+
+// function moveSlide(n) {
+//   slideIndex += n;
+//   showSlides();
+// }
+
+// function showSlides() {
+//   const slides = document.querySelectorAll('.details-container');
+//   if (slideIndex >= slides.length) { slideIndex = 0 }
+//   if (slideIndex < 0) { slideIndex = slides.length - 1 }
+//   slides.forEach(slide => {
+//     slide.style.display = 'none';
+//   });
+//   slides[slideIndex].style.display = 'block';
+// }
